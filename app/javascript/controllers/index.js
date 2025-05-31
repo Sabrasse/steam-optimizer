0e9af53fedccc8f0add0de4b84510bd43cb697a2
Keyvan Sabras <keyvan.sabras@edu.escp.eu>
2025-05-31 15:30:09 +0200

    importmap fix ?

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,6 +5,6 @@ import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 eagerLoadControllersFrom("controllers", application)
 
-// Manually register the analysis controller
-import AnalysisController from "./analysis_controller"
-application.register("analysis", AnalysisController)
+// // Manually register the analysis controller
+// import AnalysisController from "./analysis_controller"
+// application.register("analysis", AnalysisController)
