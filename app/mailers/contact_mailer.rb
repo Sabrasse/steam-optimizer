5c518c59db5021fc9e6f8bb57d075da0669b7618
Keyvan Sabras <keyvan.sabras@edu.escp.eu>
2025-07-05 15:59:37 +0200

    modal + wording changes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,11 +4,13 @@ class ContactMailer < ApplicationMailer
     @game_name = game_name
     @game_slug = game_slug
     @message = message
-    @analysis_url = show_analysis_url(game_slug: game_slug)
+    @analysis_url = game_slug.present? ? show_analysis_url(game_slug: game_slug) : nil
+    
+    subject = game_name.present? ? "New Steam Page Review Request: #{game_name}" : "New Contact Form Submission"
     
     mail(
       to: "keyvan.games@pm.me",
-      subject: "New Steam Page Review Request: #{game_name}",
+      subject: subject,
       reply_to: @email
     )
   end
